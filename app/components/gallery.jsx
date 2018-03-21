4df7c488aed99f7e9cd697b294d8f112d3d458e2
Ben Israel S. Suarez <benisraels.suarez@Estrellas-MacBook-Pro.local>
2018-03-21 00:21:52 -0400

    set up gallery comp

---
+++ app/components/gallery.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import CoverFlow from 'coverflow-react';
+
+//leveraged Coverflow npm package to produce an image carousel of Album Covers with their titles
+export default class Gallery extends React.Component {
+  render() {
+
+    let IMAGES = [];
+    let LABELS =[];
+
+    //iterates over albumResults object that was passed down via props to fill in the IMAGES and LABELS
+    if (this.props.state.albumResults) {
+      for (let key in this.props.state.albumResults) {
+        IMAGES.push(key)
+        LABELS.push(this.props.state.albumResults[key].albumName)
+      }
+    }
+
+    return (
+      <CoverFlow emptyMessage={""}height={200} background={"#ECEDEF"} itemRatio="1:1" imagesArr={IMAGES} labelsArr={LABELS}/> 
+    )
+  }
+}
